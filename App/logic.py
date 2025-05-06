2749f008d22b3327eb5e3472222003842b4426d8
dago649 <da.gomezr12@uniandes.edu.co>
2025-05-06 16:12:21 -0500

    logica

--- App/logic.py
+++ App/logic.py
@@ -71,8 +71,8 @@ def new_analyzer():
     """
     try:
         analyzer = {
-            'stops': None,
-            'connections': None,
+            'stops': m.new_map(1000, m.MAPTYPE["LINEAR_PROBING"], comp_stop),
+            'connections': gr.new_graph(directed=True),
             'components': None,
             'paths': None
         }
@@ -84,7 +84,7 @@ def new_analyzer():
         # necesarias para la carga de datos
         # ___________________________________________________
 
-        return analyzer
+        return analyzer 
     except Exception as exp:
         return exp
 
@@ -93,10 +93,59 @@ def new_analyzer():
 #  de datos en los modelos
 # ___________________________________________________
 
+def comp_stop(id1, id2):
+    return 0 if id1 == id2 else (1 if id1 < id2 else -1)
 
-def load_services(file):
+def load_services(analyzer, file):
      
-
+    try:
+        tiempo_inicio = get_time()
+        archivo = os.path.join(data_dir, file)
+        with open(archivo, "r", endcond="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            
+            secuencia_in = None
+            parada_ant = None
+            serv_act = None
+            
+            for val in reader:
+                stop_cod = val["BusStopCode"]
+                distancia = float(val["Distance"])
+                secuencia = int(val["StopSequence"])
+                direccion = f"{val["ServiceNo"]}_{val["Direction"]}"
+                
+                if not m.contains(analyzer["stops"], stop_cod):
+                    m.put(analyzer["stops"], stop_cod, {
+                        "codigo": stop_cod,
+                        "primer_bus": {
+                            "día laborable": val["WD_FirstBus"],
+                            "sábado": val["SAT_FirstBus"],
+                            "domingo": val["SUN_FirstBus"]},
+                        "último_autobús": {
+                            "día laborable": val["WD_LastBus"],
+                            "sábado": val["SAT_LastBus"],
+                            "domingo": val["SUN_LastBus"]
+                                }
+                    })
+                
+                if not gr.containsVertex(analyzer["connections"], stop_cod):
+                    gr.insertVertex(analyzer["connections"], stop_cod)
+                    
+                if parada_ant and secuencia_in == secuencia - 1 and serv_act == direccion:
+                    gr.addEdge(analyzer["connections"], parada_ant, stop_cod, distancia)
+                    
+                parada_ant = stop_cod
+                secuencia_in = secuencia
+                serv_act = direccion
+        
+        analyzer["components"] = gr.connected_components(analyzer["connections"])
+        tiempo_final = get_time()
+        print(f"Tiempo de carga: {delta_time(tiempo_final, tiempo_inicio)}ms")
+        return True
+    
+    except Exception as exp:
+        return False
+                
 
 
 
